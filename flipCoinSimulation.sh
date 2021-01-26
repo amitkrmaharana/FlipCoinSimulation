913b53288a2419ee255865d50e03c5c5af3724e5
amitkrmaharana <ak657harley@gmail.com>
2021-01-26 21:27:54 +0530

    Flipped the coin

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
+isHead=0
+isTail=1
 echo "Welcome to Flip Coin Simulation Programm"
+flipCoin=$((RANDOM%2))
+if [ $flipCoin -eq $isHead ]
+then
+	echo "Head wins the game"
+else
+	echo "Tail wins the game"
+fi
 

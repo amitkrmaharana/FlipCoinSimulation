cf48a9bbc0ea6c40b40f44091861d574c8c8b6ea
amitkrmaharana <ak657harley@gmail.com>
2021-01-26 21:34:41 +0530

    Looped the Simulator to 20 times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 isHead=0
 isTail=1
+head_count=0
+tail_count=0
 echo "Welcome to Flip Coin Simulation Programm"
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq $isHead ]
-then
-	echo "Head wins the game"
-else
-	echo "Tail wins the game"
-fi
-
+for((i=0;i<20;i++))
+do
+	flipCoin=$((RANDOM%2))
+	if [ $flipCoin -eq $isHead ]
+	then
+		(( head_count++ ))
+	else
+		(( tail_count++ ))
+	fi
+done
+echo "HEADS has won $head_count times and TAILS has won $tail_count times"
